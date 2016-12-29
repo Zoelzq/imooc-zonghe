589b72165eaaae5f95f670f01703a9c32fd6c828
ZZZZZoe <li_zhuoqing@163.com>
2016-12-29 21:28:18 +0800

    add

---
+++ js/setHomeSetFav.js
@@ -0,0 +1,50 @@
+//加入收藏
+
+function AddFavorite(sURL, sTitle)
+{
+    
+    sURL = encodeURI(sURL);
+    try
+    {
+        
+        window.external.addFavorite(sURL, sTitle);
+        
+    }
+    catch (e)
+    {
+        
+        try
+        {
+            
+            window.sidebar.addPanel(sTitle, sURL, "");
+            
+        }
+        catch (e)
+        {
+            
+            alert("您的浏览器暂时不支持收藏功能，请手动收藏！");
+            
+        }
+        
+    }
+    
+}
+
+//设为首页
+function SetHome(obj,vrl){
+    try{
+        obj.style.behavior='url(#default#homepage)';obj.setHomePage(vrl);
+    }
+    catch(e){
+        if(window.netscape) {
+            try {
+                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+            }
+            catch (e) {
+                alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将 [signed.applets.codebase_principal_support]的值设置为'true',双击即可。");
+            }
+            var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+            prefs.setCharPref('browser.startup.homepage',vrl);
+        }
+    }
+}
